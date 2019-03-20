b05729e37ab79e577ef3c2b722d68e8217dc7ff9
lilia <liliaborrazas14@gmail.com>
2019-03-20 15:18:22 +0100

    first commit

---
+++ f-online-life-calendar-liliaborrazas.git/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+         <h1 className="arrancando">arrancando</h1>
+        </header>
+        <main className="app-main">
+        
+
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
