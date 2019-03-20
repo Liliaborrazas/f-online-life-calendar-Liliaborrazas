fd606ef073d76f18f566a5434f816b808608c8e8
lilia <liliaborrazas14@gmail.com>
2019-03-20 17:29:57 +0100

    create form

--- f-online-life-calendar-liliaborrazas.git/src/App.js
+++ f-online-life-calendar-liliaborrazas.git/src/App.js
@@ -2,16 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      faces: [],
+      date: "",
+      status: "",
+      mesagge: "",
+    }
+  }
+
+
+
+  
   render() {
     return (
       <div className="app">
-        <header className="app-header">
-         <h1 className="arrancando">arrancando</h1>
-        </header>
-        <main className="app-main">
-        
-
-        </main>
+        <form className="form__container" onSubmit>
+          <div className="date__container">
+              <label name="date">Fecha</label>
+              <input className="inputDate" type="date" name="date" onChange required />
+          </div>
+          <div className="status__container">
+              <label name="status">Estado</label>
+              <label name="happy">
+              <input className="inputStatus" type="checkbox" name="happy" onChange /> :)
+              </label>
+              <label name="sad">
+              <input className="checkboxs" type="checkbox" name="sad" onChange /> :(
+              </label>
+          </div> 
+          <div className>
+              <label className="labelHappy" name="mesagge" onChange>Mensaje
+              <input className="inputhappy" type="text" name="happy" placeholder="¿Por qué es un buen día?" />
+              </label>
+          </div>
+          <div className="buttons__container">
+            <button className="btn-submit" type="submit">GUARDAR</button>
+            <button className="btn-cancel" onClick>CANCELAR</button>      
+          </div>
+		     </form>
       </div>
     );
   }
